076173d3fb30c37a07faf29a9f2435f456285ef2
Uzor Promise <uzorpromiseokwudiri@gmail.com>
2025-10-08 12:30:20 +0000

    Register tsconfig paths in api/index.js for improved module resolution and organization.

--- api/index.js
+++ api/index.js
@@ -1,5 +1,19 @@
 /* eslint-disable */
-require('tsconfig-paths/register');
+const path = require('path');
+
+// Register tsconfig paths with explicit baseUrl
+require('tsconfig-paths').register({
+  baseUrl: path.join(__dirname, '..', 'dist'),
+  paths: {
+    '@config/*': ['config/*'],
+    '@controllers/*': ['controllers/*'],
+    '@middleware/*': ['middleware/*'],
+    '@models/*': ['models/*'],
+    '@routes/*': ['routes/*'],
+    '@types/*': ['types/*'],
+    '@utils/*': ['utils/*']
+  }
+});
 
 const app = require('../dist/app.js').default;
 
